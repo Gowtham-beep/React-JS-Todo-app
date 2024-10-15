7d8d6130911907f20c9312a0a6c45446c8019e9f
Gowtham <gn86923@gmail.com>
2024-10-15 04:07:31 -0700

    Input functionality Added

--- Todo-app/src/App.jsx
+++ Todo-app/src/App.jsx
@@ -4,21 +4,27 @@ import {Tabs} from './components/Tabs'
 import { TodoList } from './components/TodoList'
 import { TodoInput } from './components/TodoInput'
 
-
-
 function App() {
-   const todos=[
-    {input:'Go to gym',completed:true},
-    {input:'Go to gym',completed:true},
-    {input:'Go to gym',completed:false},
-    {input:'Go to gym',completed:false},
-   ]
+   const [todos,setTodos]=useState([
+    {input:'Go to gym',completed:true}
+   ])
+   function addNewTodo(newTodo){
+    const newTodoList=[...todos,{input:newTodo,completed:false}]
+    setTodos(newTodoList)
+   }
+   function editTodo(){
+
+   }
+   function deleteTodo(){
+
+   }
+   
   return (
    <>
     <Header todos={todos}/>
     <Tabs todos={todos}/>
     <TodoList todos={todos}/>
-    <TodoInput/>
+    <TodoInput addNewTodo={addNewTodo}/>
    </>
   )
 }
--- Todo-app/src/components/TodoInput.jsx
+++ Todo-app/src/components/TodoInput.jsx
@@ -1,9 +1,18 @@
-export function TodoInput(){
+import {useState} from 'react'
+export function TodoInput(props){
+    const {addNewTodo}=props
+    const [inputValue,setInputValue]=useState('')
+ 
+    
     return(
         <div className="input-container">
-            <input placeholder="Add Task"/>
-            <button>
-            {/* <h4>ADD</h4> */}
+            <input placeholder="Add Task" value={inputValue} 
+            onChange={(e)=>{setInputValue(e.target.value)}}/>
+            <button onClick={()=>{
+                if(!inputValue){ return }
+                addNewTodo(inputValue)
+                setInputValue('')
+            }}>
             <i className="fa-regular fa-plus"></i>
             </button>
         </div>
